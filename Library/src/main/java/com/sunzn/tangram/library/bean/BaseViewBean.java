d6c3fb706a9a3adc57f8ce40ab89c54343233d81
sunzn <sunzn.cn@gmail.com>
2018-03-21 17:18:37 +0800

    Update BaseViewBean

--- Library/src/main/java/com/sunzn/tangram/library/bean/BaseViewBean.java
+++ Library/src/main/java/com/sunzn/tangram/library/bean/BaseViewBean.java
@@ -1,11 +1,38 @@
 package com.sunzn.tangram.library.bean;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by sunzn on 2017/9/5.
  */
 
-public class BaseViewBean {
+public class BaseViewBean implements Parcelable {
 
-    // TODO
+    protected BaseViewBean(Parcel in) {
+        // TODO
+    }
 
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        // TODO
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<BaseViewBean> CREATOR = new Creator<BaseViewBean>() {
+        @Override
+        public BaseViewBean createFromParcel(Parcel in) {
+            return new BaseViewBean(in);
+        }
+
+        @Override
+        public BaseViewBean[] newArray(int size) {
+            return new BaseViewBean[size];
+        }
+    };
+    
 }
