5dd39ce0a8465dfb0c6c9f20f116555a8ae10e21
sunzn <sunzn.cn@gmail.com>
2018-03-21 13:49:32 +0800

    Update Line

---
+++ Sample/src/main/java/com/sunzn/tangram/sample/bean/Line.java
@@ -0,0 +1,16 @@
+package com.sunzn.tangram.sample.bean;
+
+import com.sunzn.tangram.library.annotation.LayoutAnnotation;
+import com.sunzn.tangram.library.bean.BaseViewBean;
+import com.sunzn.tangram.sample.R;
+
+/**
+ * Created by sunzn on 2017/10/17.
+ */
+
+@LayoutAnnotation(R.layout.line)
+public class Line extends BaseViewBean {
+
+    // TODO
+
+}
