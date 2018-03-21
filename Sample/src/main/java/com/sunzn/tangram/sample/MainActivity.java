a8dea04a3128473d811def6a50dfb50f2e1169a1
sunzn <sunzn.cn@gmail.com>
2018-03-21 13:51:00 +0800

    Update MainActivity

--- Sample/src/main/java/com/sunzn/tangram/sample/MainActivity.java
+++ Sample/src/main/java/com/sunzn/tangram/sample/MainActivity.java
@@ -3,16 +3,21 @@ package com.sunzn.tangram.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.sunzn.tangram.library.bean.BaseViewBean;
+import com.sunzn.tangram.library.interfaces.FooterClickListener;
 import com.sunzn.tangram.library.interfaces.LoadMoreListener;
 import com.sunzn.tangram.library.view.RecyclerCompatView;
 import com.sunzn.tangram.sample.bean.Done;
 import com.sunzn.tangram.sample.bean.Foot;
 import com.sunzn.tangram.sample.bean.Head;
 import com.sunzn.tangram.sample.bean.Item;
+import com.sunzn.tangram.sample.bean.Line;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,22 +37,34 @@ public class MainActivity extends AppCompatActivity {
 
         beans = new ArrayList<>();
         for (int i = 0; i < 1; i++) {
-            beans.add(new Head());
-        }
-        for (int i = 0; i < 9; i++) {
-            beans.add(new Item());
-        }
-        for (int i = 0; i < 2; i++) {
-            beans.add(new Foot());
+            beans.add(new Line());
         }
         for (int i = 0; i < 1; i++) {
-            beans.add(new Head());
+            beans.add(new Head("今日快讯"));
+        }
+        for (int i = 0; i < 5; i++) {
+            beans.add(new Item());
+        }
+        for (int i = 0; i < 1; i++) {
+            beans.add(new Line());
+        }
+        for (int i = 0; i < 1; i++) {
+            beans.add(new Head("期刊"));
         }
         for (int i = 0; i < 9; i++) {
             beans.add(new Done());
         }
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 1; i++) {
+            beans.add(new Line());
+        }
+        for (int i = 0; i < 1; i++) {
+            beans.add(new Head("知网书"));
+        }
+        for (int i = 0; i < 4; i++) {
             beans.add(new Foot());
+        }
+        for (int i = 0; i < 1; i++) {
+            beans.add(new Line());
         }
         for (int i = 0; i < 1; i++) {
             beans.add(new Item());
@@ -92,6 +109,17 @@ public class MainActivity extends AppCompatActivity {
         rv.setLayoutManager(new GridLayoutManager(this, 6));
 
         adapter = new RecyclerAdapter(beans);
+
+
+        adapter.setFooterClickListener(new FooterClickListener() {
+            @Override
+            public void onFooterClick() {
+                rv.setLoading();
+                loadData();
+            }
+        });
+
+
         rv.setCompatAdapter(adapter);
 
         rv.setLoadMoreListener(new LoadMoreListener() {
@@ -99,29 +127,52 @@ public class MainActivity extends AppCompatActivity {
             public void onLoadMore() {
 
                 if (x < 5) {
-
-                    x++;
-
-                    final ArrayList<Done> s = new ArrayList<Done>();
-
-                    for (int i = 0; i < 9; i++) {
-                        s.add(new Done());
-                    }
-
-                    rv.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            rv.setLoadFinish(s);
-                        }
-                    }, 2000);
-
-
+                    loadData();
                 } else {
-                    rv.setTerminal();
+                    rv.setClosure();
+                    Log.i("Tangram", "x ================================================== " + x);
                 }
 
             }
         });
+
     }
-    
+
+    private void loadData() {
+
+        final ArrayList<BaseViewBean> s = new ArrayList<>();
+
+        Random random = new java.util.Random();
+        int result = random.nextInt(10);
+
+        if (result % 3 == 0) {
+            for (int i = 0; i < 9; i++) {
+                s.add(new Done());
+            }
+        } else {
+            for (int i = 0; i < 6; i++) {
+                s.add(new Foot());
+            }
+        }
+
+        rv.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Random random = new java.util.Random();
+                int result = random.nextInt(10);
+
+                Log.i("Tangram", "i ======================================================================== " + result);
+
+                if (result % 3 == 0) {
+                    rv.setFailure();
+                } else {
+                    rv.setSuccess(s);
+                    x++;
+                }
+            }
+        }, 1000);
+
+    }
+
 }
