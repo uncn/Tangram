c6484ffab840970b78da4d75b505d929c57de601
sunzn <sunzn.cn@gmail.com>
2018-03-22 11:39:53 +0800

    Serializable BaseViewBean

--- Library/src/main/java/com/sunzn/tangram/library/bean/BaseViewBean.java
+++ Library/src/main/java/com/sunzn/tangram/library/bean/BaseViewBean.java
@@ -1,38 +1,13 @@
 package com.sunzn.tangram.library.bean;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import java.io.Serializable;
 
 /**
  * Created by sunzn on 2017/9/5.
  */
 
-public class BaseViewBean implements Parcelable {
+public class BaseViewBean implements Serializable {
 
-    protected BaseViewBean(Parcel in) {
-        // TODO
-    }
+    // TODO
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        // TODO
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<BaseViewBean> CREATOR = new Creator<BaseViewBean>() {
-        @Override
-        public BaseViewBean createFromParcel(Parcel in) {
-            return new BaseViewBean(in);
-        }
-
-        @Override
-        public BaseViewBean[] newArray(int size) {
-            return new BaseViewBean[size];
-        }
-    };
-    
 }
