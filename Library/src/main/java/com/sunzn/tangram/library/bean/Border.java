aa30c2f1ff35243b9ea59ca4bb0a6fa14517cf2f
sunzn <sunzn.cn@gmail.com>
2018-08-29 14:19:00 +0800

    Update Library

---
+++ Library/src/main/java/com/sunzn/tangram/library/bean/Border.java
@@ -0,0 +1,69 @@
+package com.sunzn.tangram.library.bean;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+public class Border implements Serializable, Parcelable {
+
+    private int lower;
+
+    private int upper;
+
+    public Border() {}
+
+    public Border(int lower, int upper) {
+        this.lower = lower;
+        this.upper = upper;
+    }
+
+    public int getLower() {
+        return lower;
+    }
+
+    public void setLower(int lower) {
+        this.lower = lower;
+    }
+
+    public int getUpper() {
+        return upper;
+    }
+
+    public void setUpper(int upper) {
+        this.upper = upper;
+    }
+
+    public boolean isInRange(int position) {
+        return position >= lower && position < upper;
+    }
+
+    private Border(Parcel in) {
+        lower = in.readInt();
+        upper = in.readInt();
+    }
+
+    public static final Creator<Border> CREATOR = new Creator<Border>() {
+        @Override
+        public Border createFromParcel(Parcel in) {
+            return new Border(in);
+        }
+
+        @Override
+        public Border[] newArray(int size) {
+            return new Border[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(lower);
+        dest.writeInt(upper);
+    }
+
+}
