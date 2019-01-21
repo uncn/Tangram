c95ebc4872275bd99fc4c24c923af4cc47d29556
sunzn <sunzn.cn@gmail.com>
2019-01-21 11:45:07 +0800

    Update Sample

--- Sample/src/main/java/com/sunzn/tangram/sample/MainActivity.java
+++ Sample/src/main/java/com/sunzn/tangram/sample/MainActivity.java
@@ -1,10 +1,14 @@
 package com.sunzn.tangram.sample;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 
+import com.sunzn.tangram.library.bean.Border;
 import com.sunzn.tangram.library.bean.TangramBean;
 import com.sunzn.tangram.library.interfaces.FooterClickListener;
 import com.sunzn.tangram.library.interfaces.LoadMoreListener;
@@ -50,17 +54,28 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 1; i++) {
             beans.add(new Head("期刊"));
         }
-        for (int i = 0; i < 9; i++) {
-            beans.add(new Done());
+
+
+        Log.e("TAGGG", "size" + beans.size());
+
+        Border border = new Border(beans.size(), beans.size() + 10);
+
+        for (int i = 0; i < 10; i++) {
+            beans.add(new Done(border));
         }
+
+        Log.e("TAGGG", "size" + beans.size());
+
         for (int i = 0; i < 1; i++) {
             beans.add(new Line());
         }
         for (int i = 0; i < 1; i++) {
             beans.add(new Head("知网书"));
         }
+
+        Border border1 = new Border(beans.size(), beans.size() + 10);
         for (int i = 0; i < 4; i++) {
-            beans.add(new Foot());
+            beans.add(new Foot(border1));
         }
         for (int i = 0; i < 1; i++) {
             beans.add(new Line());
@@ -68,39 +83,44 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 1; i++) {
             beans.add(new Item());
         }
+
+        Border border2 = new Border(beans.size(), beans.size() + 9);
         for (int i = 0; i < 9; i++) {
-            beans.add(new Done());
+            beans.add(new Done(border2));
+        }
+
+        for (int i = 0; i < 1; i++) {
+            beans.add(new Item());
+        }
+
+        Border border3 = new Border(beans.size(), beans.size() + 9);
+        for (int i = 0; i < 9; i++) {
+            beans.add(new Done(border3));
         }
         for (int i = 0; i < 1; i++) {
             beans.add(new Item());
         }
-        for (int i = 0; i < 9; i++) {
-            beans.add(new Done());
-        }
+//        for (int i = 0; i < 9; i++) {
+//            beans.add(new Done());
+//        }
         for (int i = 0; i < 1; i++) {
             beans.add(new Item());
         }
-        for (int i = 0; i < 9; i++) {
-            beans.add(new Done());
-        }
+//        for (int i = 0; i < 9; i++) {
+//            beans.add(new Done());
+//        }
         for (int i = 0; i < 1; i++) {
             beans.add(new Item());
         }
-        for (int i = 0; i < 9; i++) {
-            beans.add(new Done());
-        }
+//        for (int i = 0; i < 9; i++) {
+//            beans.add(new Done());
+//        }
         for (int i = 0; i < 1; i++) {
             beans.add(new Item());
         }
-        for (int i = 0; i < 9; i++) {
-            beans.add(new Done());
-        }
-        for (int i = 0; i < 1; i++) {
-            beans.add(new Item());
-        }
-        for (int i = 0; i < 9; i++) {
-            beans.add(new Done());
-        }
+//        for (int i = 0; i < 9; i++) {
+//            beans.add(new Done());
+//        }
 
 //        rv.setLayoutManager(new LinearLayoutManager(this));
 
@@ -119,6 +139,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        tangram.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                int position = parent.getChildAdapterPosition(view);
+                switch (adapter.getItemViewType(position)) {
+
+                    case R.layout.done:
+//                        Point point = ((Base) adapter.getItem(position)).getPoint();
+//                        if (position >= point.x && position < point.y) {
+//                            int spanCount = 3;
+//                            int hSpacing = 40;
+//                            int vSpacing = 15;
+//                            int column = (position - point.x) % spanCount;
+//                            outRect.left = hSpacing - column * hSpacing / spanCount;
+//                            outRect.right = (column + 1) * hSpacing / spanCount;
+////                            if (position < spanCount) {
+//                            outRect.top = vSpacing;
+////                            }
+//                            outRect.bottom = vSpacing;
+//                        }
+                        break;
+                    case R.layout.foot:
+//                        Point pointFoot = ((Base) adapter.getItem(position)).getPoint();
+//                        if (position >= pointFoot.x && position < pointFoot.y) {
+//                            int spanCount = 2;
+//                            int hSpacing = 40;
+//                            int vSpacing = 15;
+//                            int column = (position - pointFoot.x) % spanCount;
+//                            outRect.left = hSpacing - column * hSpacing / spanCount;
+//                            outRect.right = (column + 1) * hSpacing / spanCount;
+////                            if (position < spanCount) {
+//                            outRect.top = vSpacing;
+////                            }
+//                            outRect.bottom = vSpacing;
+//                        }
+                        break;
+                    default:
+                        outRect.left = 0;
+                        outRect.right = 0;
+                        outRect.bottom = 0;
+                        outRect.top = 0;
+                        break;
+
+                }
+            }
+        });
 
         tangram.setCompatAdapter(adapter);
 
@@ -146,12 +213,14 @@ public class MainActivity extends AppCompatActivity {
         int result = random.nextInt(10);
 
         if (result % 3 == 0) {
+            Border border = new Border(beans.size(), beans.size() + 9);
             for (int i = 0; i < 9; i++) {
-                s.add(new Done());
+                s.add(new Done(border));
             }
         } else {
+            Border border = new Border(beans.size(), beans.size() + 6);
             for (int i = 0; i < 6; i++) {
-                s.add(new Foot());
+                s.add(new Foot(border));
             }
         }
 
