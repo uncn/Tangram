8eaa294b679c67574f6ad67a8fa3c18f3d0fe4ab
sunzn <sunzn.cn@gmail.com>
2019-04-29 16:04:21 +0800

    Update TangramHelper

--- Library/src/main/java/com/sunzn/tangram/library/helper/TangramHelper.java
+++ Library/src/main/java/com/sunzn/tangram/library/helper/TangramHelper.java
@@ -9,18 +9,20 @@ public class TangramHelper {
 
     public static <R extends TangramBean, T extends R> void addTangramBean(List<R> beans, T bean) {
         if (beans != null && bean != null) {
-            Border border = new Border(beans.size(), beans.size() + 1);
-            bean.setBorder(border);
+            int lower = beans.size();
+            int upper = beans.size() + 1;
+            bean.setBorder(new Border(lower, upper, 0));
             beans.add(bean);
         }
     }
 
     public static <R extends TangramBean, T extends R> void addTangramBean(List<R> beans, List<T> data) {
         if (beans != null && data != null && data.size() > 0) {
-            Border border = new Border(beans.size(), beans.size() + data.size());
+            int lower = beans.size();
+            int upper = beans.size() + data.size();
             for (int i = 0; i < data.size(); i++) {
                 T bean = data.get(i);
-                bean.setBorder(border);
+                bean.setBorder(new Border(lower, upper, i));
                 beans.add(bean);
             }
         }
