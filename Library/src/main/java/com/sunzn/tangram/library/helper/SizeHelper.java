0f67155f261527d67b711d832dddd9d834a293a6
sunzn <sunzn.cn@gmail.com>
2019-05-28 09:15:01 +0800

    Update SizeHelper

--- Library/src/main/java/com/sunzn/tangram/library/helper/SizeHelper.java
+++ Library/src/main/java/com/sunzn/tangram/library/helper/SizeHelper.java
@@ -1,15 +1,12 @@
 package com.sunzn.tangram.library.helper;
 
-import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
 
 public class SizeHelper {
 
-    public static int dp2px(Context context, int dp) {
-        return (int) (getDensity(context) * dp + 0.5);
-    }
-
-    public static float getDensity(Context context) {
-        return context.getResources().getDisplayMetrics().density;
+    public static int dp2px(float dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
     }
 
 }
